5fa7a639d9352d097d1e70548ff06fbadb5629ab
Matthew Olivo <saviorxtanren@gmail.com>
2020-01-18 16:47:52 -0800

    Revert "- Initial start of new settings v2 files"
    This reverts commit c99e2283b9059fb58550f8c4f9e4d0ad732b4d16.

--- MixItUp.Base/Model/Settings/ModeratorSettingsV2Model.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.Serialization;
-
-namespace MixItUp.Base.Model.Settings
-{
-    [DataContract]
-    public class ModeratorSettingsV2Model : BaseSettingsV2ModelBase
-    {
-    }
-}
